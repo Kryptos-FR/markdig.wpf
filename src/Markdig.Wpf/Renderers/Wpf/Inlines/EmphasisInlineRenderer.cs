a9819f3315750a1446229f43627cfd1b5f896f23
Kryptos-FR <musset.nicolas@gmail.com>
2016-06-26 21:33:56 +0900

    [Markdig.Wpf] Partially implemented the emphasis inline renderer. Still need to figure out how to add the children to the inline, maybe with a stack.

--- src/Markdig.Wpf/Renderers/Wpf/Inlines/EmphasisInlineRenderer.cs
+++ src/Markdig.Wpf/Renderers/Wpf/Inlines/EmphasisInlineRenderer.cs
@@ -2,16 +2,60 @@
 // This file is licensed under the MIT license. 
 // See the LICENSE.md file in the project root for more information.
 
-using System;
+using System.Windows.Documents;
 using Markdig.Syntax.Inlines;
 
 namespace Markdig.Renderers.Wpf.Inlines
 {
+    using Inline = System.Windows.Documents.Inline;
+
+    /// <summary>
+    /// A WPF renderer for an <see cref="EmphasisInline"/>.
+    /// </summary>
+    /// <seealso cref="Markdig.Renderers.Wpf.WpfObjectRenderer{Markdig.Syntax.Inlines.EmphasisInline}" />
     public class EmphasisInlineRenderer : WpfObjectRenderer<EmphasisInline>
     {
+        /// <summary>
+        /// Delegates to get the inline associated to an <see cref="EmphasisInline"/> object.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns>The WPF inline associated to this <see cref="EmphasisInline"/> object</returns>
+        public delegate Inline GetInlineDelegate(EmphasisInline obj);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmphasisInlineRenderer"/> class.
+        /// </summary>
+        public EmphasisInlineRenderer()
+        {
+            GetInline = GetDefaultInline;
+        }
+
+        /// <summary>
+        /// Gets or sets the GetInline delegate.
+        /// </summary>
+        public GetInlineDelegate GetInline { get; set; }
+
+        /// <summary>
+        /// Gets the default WPF inline for ** and __ emphasis.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns></returns>
+        public static Inline GetDefaultInline(EmphasisInline obj)
+        {
+            if (obj.DelimiterChar == '*' || obj.DelimiterChar == '_')
+            {
+                return obj.IsDouble ? (Inline)new Bold() : new Italic();
+            }
+            return null;
+        }
+
         protected override void Write(WpfRenderer renderer, EmphasisInline obj)
         {
-            throw new NotImplementedException();
+            var inline = GetInline(obj);
+            // TODO: "render" the inline
+            renderer.WriteChildren(obj);
+            // TODO: add children to the inline
+            // Note: could be a push/pop API with a stack. Pop automatically adds children into the inline and removes it from the stack.
         }
     }
 }
