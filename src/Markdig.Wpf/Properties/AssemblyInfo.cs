2af4fb074b133a9af17bb5c7f9d394036521de6a
Kryptos-FR <musset.nicolas@gmail.com>
2016-06-26 15:37:11 +0900

    [General] Created VS projects and solution.

---
+++ src/Markdig.Wpf/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Markdig.Wpf")]
+[assembly: AssemblyDescription("a WPF port to CommonMark compliant Markdig.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Markdig.Wpf")]
+[assembly: AssemblyCopyright("Copyright © Nicolas Musset 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion(Markdig.Wpf.Version)]
+[assembly: AssemblyFileVersion(Markdig.Wpf.Version)]
+
+namespace Markdig
+{
+    public static partial class Wpf
+    {
+        public const string Version = "0.0.1";
+    }
+}
