324cd84e62b0da929d52cb9d42b83c8e6adff680
Nicolas Musset <musset.nicolas@gmail.com>
2016-07-03 18:28:48 +0900

    [Markdig.Wpf] Implemented paragraph renderer.

--- src/Markdig.Wpf/Renderers/Wpf/ParagraphRenderer.cs
+++ src/Markdig.Wpf/Renderers/Wpf/ParagraphRenderer.cs
@@ -2,16 +2,23 @@
 // This file is licensed under the MIT license. 
 // See the LICENSE.md file in the project root for more information.
 
-using System;
 using Markdig.Syntax;
+using System.Windows.Documents;
 
 namespace Markdig.Renderers.Wpf
 {
     public class ParagraphRenderer : WpfObjectRenderer<ParagraphBlock>
     {
+        /// <inheritdoc/>
         protected override void Write(WpfRenderer renderer, ParagraphBlock obj)
         {
-            throw new NotImplementedException();
+            var paragraph = new Paragraph();
+
+            renderer.BeginBlock(paragraph);
+            renderer.WriteLeafInline(obj);
+
+            paragraph.Inlines.AddRange(renderer.Inlines);
+            renderer.EndBlock();
         } 
     }
 }
--- src/Markdig.Wpf/Renderers/WpfRenderer.cs
+++ src/Markdig.Wpf/Renderers/WpfRenderer.cs
@@ -2,16 +2,20 @@
 // This file is licensed under the MIT license. 
 // See the LICENSE.md file in the project root for more information.
 
+using System.Collections.Generic;
 using System.Windows.Documents;
 using Markdig.Helpers;
 using Markdig.Renderers.Wpf;
 using Markdig.Renderers.Wpf.Inlines;
 using System.Runtime.CompilerServices;
+using System;
 
 namespace Markdig.Renderers
 {
     public class WpfRenderer : RendererBase
     {
+        private readonly Stack<Block> blocks = new Stack<Block>();
+        private readonly List<Inline> inlines = new List<Inline>();
         private char[] buffer;
 
         public WpfRenderer(FlowDocument document)
@@ -24,7 +28,7 @@ namespace Markdig.Renderers
             //ObjectRenderers.Add(new ListRenderer());
             //ObjectRenderers.Add(new HeadingRenderer());
             //ObjectRenderers.Add(new HtmlBlockRenderer());
-            //ObjectRenderers.Add(new ParagraphRenderer());
+            ObjectRenderers.Add(new ParagraphRenderer());
             //ObjectRenderers.Add(new QuoteBlockRenderer());
             //ObjectRenderers.Add(new ThematicBreakRenderer());
 
@@ -42,38 +46,86 @@ namespace Markdig.Renderers
 
         public FlowDocument Document { get; }
 
-        public WpfRenderer WriteBlock(Block block)
+        internal IReadOnlyCollection<Inline> Inlines => inlines;
+
+        /// <inheritdoc/>
+        public override object Render(Syntax.MarkdownObject markdownObject)
         {
-            // TODO
-            return this;
+            Write(markdownObject);
+            return Document;
         }
 
-        public WpfRenderer WriteInline(Inline inline)
+        /// <summary>
+        /// Writes the inlines of a leaf inline.
+        /// </summary>
+        /// <param name="leafBlock">The leaf block.</param>
+        /// <returns>This instance</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void WriteLeafInline(Syntax.LeafBlock leafBlock)
+        {
+            if (leafBlock == null) throw new ArgumentNullException(nameof(leafBlock));
+            var inline = (Syntax.Inlines.Inline)leafBlock.Inline;
+            if (inline != null)
+            {
+                while (inline != null)
+                {
+                    Write(inline);
+                    inline = inline.NextSibling;
+                }
+            }
+        }
+
+        internal void BeginBlock(Block block)
+        {
+            blocks.Push(block);
+        }
+
+        internal void EndBlock()
+        {
+            var block = blocks.Pop();
+            if (blocks.Count == 0)
+            {
+                // first-level block
+                Document.Blocks.Add(block);
+            }
+            inlines.Clear();
+        }
+
+        internal void BeginSpan(Span span)
         {
             // TODO
-            return this;
+        }
+
+        internal void EndSpan()
+        {
+            // TODO
+        }
+
+        internal void WriteInline(Inline inline)
+        {
+            inlines.Add(inline);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public WpfRenderer WriteText(ref StringSlice slice)
+        internal void WriteText(ref StringSlice slice)
         {
             if (slice.Start > slice.End)
-                return this;
+                return;
 
-            return WriteText(slice.Text, slice.Start, slice.Length);
+            WriteText(slice.Text, slice.Start, slice.Length);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public WpfRenderer WriteText(string text)
+        internal void WriteText(string text)
         {
-            return WriteInline(new Run(text));
+            WriteInline(new Run(text));
         }
 
-        public WpfRenderer WriteText(string text, int offset, int length)
+        internal void WriteText(string text, int offset, int length)
         {
             if (text == null)
-                return this;
-            
+                return;
+
             if (offset == 0 && text.Length == length)
             {
                 WriteText(text);
@@ -91,14 +143,7 @@ namespace Markdig.Renderers
                     WriteText(new string(buffer, 0, length));
                 }
             }
-            return this;
         }
 
-        /// <inheritdoc/>
-        public override object Render(Syntax.MarkdownObject markdownObject)
-        {
-            Write(markdownObject);
-            return Document;
-        }
     }
 }
