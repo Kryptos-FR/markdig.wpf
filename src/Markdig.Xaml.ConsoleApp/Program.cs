48f53e5cf297acea614e4c6b73d948705b7487c5
Nicolas Musset <musset.nicolas@gmail.com>
2016-07-09 16:59:04 +0900

    [General] Added project Markdig.Xaml.ConsoleApp

---
+++ src/Markdig.Xaml.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2016 Nicolas Musset. All rights reserved.
+// This file is licensed under the MIT license. 
+// See the LICENSE.md file in the project root for more information.
+
+using System;
+
+namespace Markdig.Xaml.ConsoleApp
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
