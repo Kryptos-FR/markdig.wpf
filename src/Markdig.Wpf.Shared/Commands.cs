e52c7a3e582a4986f4c575a1b10594cb6802fd99
Nicolas Musset <musset.nicolas@gmail.com>
2016-07-17 15:03:28 +0900

    [Markdig.Wpf.Shared] Added support for hyperlink command.

---
+++ src/Markdig.Wpf.Shared/Commands.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Input;
+
+namespace Markdig.Wpf
+{
+    public static class Commands
+    {
+        public static RoutedCommand Hyperlink { get; } = new RoutedCommand(nameof(Hyperlink), typeof(Commands));
+    }
+}
