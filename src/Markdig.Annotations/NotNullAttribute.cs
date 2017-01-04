f8b91197c0cfb8eae7169c9f897be3f87b7f6409
Nicolas Musset <musset.nicolas@gmail.com>
2017-01-04 16:23:51 +0900

    [General] Added shared project Markdig.Annotations.

---
+++ src/Markdig.Annotations/NotNullAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Markdig.Annotations
+{
+    /// <summary>
+    /// Indicates that the value of the marked element could never be <c>null</c>.
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// [NotNull] object Foo() {
+    ///   return null; // Warning: Possible 'null' assignment
+    /// }
+    /// </code>
+    /// </example>
+    [AttributeUsage(
+        AttributeTargets.Method | AttributeTargets.Parameter | AttributeTargets.Property |
+        AttributeTargets.Delegate | AttributeTargets.Field | AttributeTargets.Event |
+        AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.GenericParameter)]
+    internal sealed class NotNullAttribute : Attribute
+    {
+    }
+}
